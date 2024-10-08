9d338fcd0a7cc0d8ffa5b3d731506b2a698a132a
sungals91 <sungals911@gmail.com>
2024-10-08 10:56:29 +0900

    기본 구조 생성

---
+++ Drill 7.py
@@ -0,0 +1,39 @@
+import random
+from pico2d import *
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+def reset_world():
+    global running
+
+    running = True
+    pass
+
+def render_world():
+    pass
+
+def update_world():
+    pass
+
+open_canvas()
+
+# initialization code
+reset_world()
+
+# game main loop code
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
+# finalization code
+
+close_canvas()
