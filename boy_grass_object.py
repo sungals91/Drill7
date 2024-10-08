f66daac0c7fada465c5a11e06b296380b0431fe2
sungals91 <sungals911@gmail.com>
2024-10-08 10:27:40 +0900

    Grass 클래스 추가

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,16 +1,32 @@
 from pico2d import *
 
 # Game object class here
+class Grass:
+    # 생성자 함수: 객체의 초기 상태를 설정, 메모리 할당
+    def __init__(self):
+        # 모양 없는 납작한 붕어빵의 초기모습 결정
+        self.image = load_image('grass.png')
+    def update(self):
+        pass
+    def draw(self):
+        self.image.draw(400,30)
+    pass
 
-def  reset_world():
+
+def reset_world():
     global running
+    global grass
+
     running = True
+    grass = Grass() # 잔디를 찍어낸다
 
 def update_world():
+    grass.update() # Grass 상태를 업데이트
     pass
 
 def render_world():
     clear_canvas()
+    grass.draw()
     update_canvas()
 
 def handle_events():
